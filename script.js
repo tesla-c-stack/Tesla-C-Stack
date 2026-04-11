d404b6a0cabf6570ac896cc6f315e4add32a7bab
tesla-c-stack <elonreevesmuskextopfans@gmail.com>
2026-04-11 15:59:32 +0100

    Update script.js
    Update full structure script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-console.log("Tesla site loaded");
+function setCar(name, price, img) {
+  document.getElementById("carName").innerText = name;
+  document.getElementById("price").innerText = price;
+  document.getElementById("carImg").src = img;
+}
