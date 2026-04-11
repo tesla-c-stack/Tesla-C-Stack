c55b952c32bf2a087cba3ede664fbfb4e4ab6ed2
tesla-c-stack <elonreevesmuskextopfans@gmail.com>
2026-04-11 16:16:15 +0100

    Update script.js
    Update full structured script.js

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
+let images = [
+  "https://images.unsplash.com/photo-1617788138017-80ad40651399?auto=format&fit=crop&w=800&q=80",
+  "https://images.unsplash.com/photo-1503376780353-7e6692767b70?auto=format&fit=crop&w=800&q=80"
+];
+
 function setCar(name, price, img) {
   document.getElementById("carName").innerText = name;
   document.getElementById("price").innerText = price;
   document.getElementById("carImg").src = img;
+}
+
+function changeColor(index) {
+  document.getElementById("carImg").src = images[index];
 }
